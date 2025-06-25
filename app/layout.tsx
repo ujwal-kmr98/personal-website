32660c972a89cc776197b078f86826558d1bb7e5
Ujwal Kumar <ujwalkkumar9@gmail.com>
2025-06-25 23:39:26 +0400

    Update website title in browser tab

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
+  title: 'Ujwal Kumar - Partnership Specialist and Fintech Expert.',
   description: 'Created with v0',
   generator: 'v0.dev',
 }
